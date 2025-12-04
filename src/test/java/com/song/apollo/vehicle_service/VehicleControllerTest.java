9e4f6e484da95da8c0e57575f7f4a605cf939708
tir.song <ts667@cornell.edu>
2025-12-04 16:38:40 -0500

    test: update VehicleControllerTest imports to reflect new dependencies

--- src/test/java/com/song/apollo/vehicle_service/VehicleControllerTest.java
+++ src/test/java/com/song/apollo/vehicle_service/VehicleControllerTest.java
@@ -3,18 +3,18 @@ package com.song.apollo.vehicle_service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
-import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.boot.test.autoconfigure.*;
 
 @WebMvcTest(VehicleController.class)
 public class VehicleControllerTest {
@@ -25,8 +25,7 @@ public class VehicleControllerTest {
     @MockitoBean
     private VehicleService vehicleService;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     void createVehicle_ShouldReturn201_WhenValid() throws Exception {
@@ -42,28 +41,33 @@ public class VehicleControllerTest {
 
         when(vehicleService.createVehicle(any())).thenReturn(v);
 
-        mockMvc.perform(post("/vehicle"))
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(v))
-                .andExpect(status().isUnprocessableEntity());
+        mockMvc.perform(post("/vehicle")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(v)))
+                .andExpect(status().isCreated());
     }
 
     @Test
     void createVehicle_ShouldReturn422_WhenPriceIsNegative() throws Exception {
         Vehicle v = Vehicle.builder()
                 .vin("V123")
+                .manufacturerName("Ford")
+                .description("Desc")
+                .horsePower(100)
+                .modelName("Fiesta")
+                .fuelType(Vehicle.FuelType.GASOLINE)
                 .purchasePrice(BigDecimal.valueOf(-100))
                 .build();
-        mockMvc.perform(post("/vehicle"))
+        mockMvc.perform(post("/vehicle")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(v))
+                .content(objectMapper.writeValueAsString(v)))
                 .andExpect(status().isUnprocessableEntity());
     }
 
     @Test
     void deleteVehicle_ShouldReturn204() throws Exception {
         mockMvc.perform(delete("/vehicle/V123"))
-                .andExpect(status().isNoContent());\
+                .andExpect(status().isNoContent());
     }
 
 
